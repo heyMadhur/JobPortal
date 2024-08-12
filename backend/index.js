b171a313e686233f60ef838887c8468f8df12985
heyMadhur <madhurgupta12112002@gmail.com>
2024-08-13 00:59:47 +0530

    Connect DB

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,19 @@
 import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config({})       //  Loads environment variables from a .env file in the project’s root directory.
 
 const app= express();
+
+// app.get("/", (req, res)=>{
+//     return res.status(200).json({
+//         message: "Welcome to the home page",
+//         success: true
+//     })
+// })
 
 // Adding Middlewares
 app.use(express.json());
@@ -16,8 +27,29 @@ app.use(cors(corOptions));
 
 
 
-const PORT= 3000
+const PORT= process.env.PORT || 3000;
 app.listen(PORT, ()=>{
+    connectDB();
     console.log(`The server is running at port: ${PORT}`);
     
 })
+
+
+
+// MiddleWare Information
+// 1. express.json() - This middleware parses incoming requests with JSON payloads and makes the data available in req.body.
+
+// 2. UrlEncoded
+// Purpose: This middleware parses incoming requests with URL-encoded payloads (usually from HTML forms) and makes the data available in req.body.
+// Extended Option:
+//     extended: true: Allows parsing of nested objects (e.g., person[name]=John).
+//     extended: false: Only allows parsing of simple key-value pairs.
+// When to Use: Use this when your application needs to handle form submissions or data encoded in URLs.
+
+// 3. Cookie Parser: This middleware parses the Cookie header from the incoming request and populates req.cookies with an object keyed by cookie names.
+
+// 4. Cors Options
+// Purpose: This line applies the CORS (Cross-Origin Resource Sharing) middleware with the specified options (corOptions).
+// When to Use: Use this when your server needs to handle requests from a different origin, such as when your frontend is served from a different domain or port than your backend. CORS allows the server to specify which origins are permitted to access its resources.
+// Example:
+// If your frontend is running on http://localhost:5173 and your backend on http://localhost:3000, CORS ensures that requests made from the frontend to the backend are allowed, considering the same-origin policy enforced by browsers.
