0d4c7cc8b884253571240b2dd21ae6d66c363af2
heyMadhur <madhurgupta12112002@gmail.com>
2024-08-15 12:43:37 +0530

    NavBar Basic Structure Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Navbar from './components/shared/Navbar'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-red-500'>Starting Template</h1>
+      {/* <h1 className='text-red-500'>Starting Template</h1> */}
+      <Navbar />
     </>
   )
 }
