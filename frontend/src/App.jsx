5ed1a6339b2e978d2b85ca9feb84d1fde711b204
heyMadhur <madhurgupta12112002@gmail.com>
2024-08-15 10:29:19 +0530

    Final Template with Vite, React, Tailwind, Shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <h1>Starting Template</h1>
+      <h1 className='text-red-500'>Starting Template</h1>
     </>
   )
 }
