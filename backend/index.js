143bdfd5449be6c5f2b6bc3e2c3f9f2c99a7776e
heyMadhur <madhurgupta12112002@gmail.com>
2024-08-22 13:21:58 +0530

    Vercel API Changes  in Front End and Origin changes in backend

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://job-portal-frontend-mocha.vercel.app',
     credentials: true
 }
 app.use(cors(corOptions));
